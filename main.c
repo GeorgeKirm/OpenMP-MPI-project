391501f480b442643efb064cdd5b7198a7b20cea
GeorgeKirm <GeorgeKirm@users.noreply.github.com>
2016-03-11 02:23:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,49 @@
 #include "main.h"
 
 /*
-*
 * char *argv[] parameters:
 *  argv[1]: max limit to examine or -1 to mark no limit
 *  argv[2]: max limit time to examine coordinates or -1 to mark no limit
 *  argv[3]: File's name
-*  argv[4]: Thread that used or -1 to mark no limit
+*  argv[4]: Thread that are being used or -1 to mark no limit
 *  argv[5]: Proccesses to use MPI or -1 to mark no limit
-*
 */
 int main (int argc, char *argv[]) {
-    int tid;
+	if(argc!=6) {
+		printf("Wrong number of arguents");
+	}
+	int tid; //thread id
 	FILE *readFile;
 	readFile = fopen(argv[1],"r");
-	struct timespec startTime, endTime;
+	if(readFile==NULL) { //exit file if there is no data file
+		printf("Error while opening the file. Run generator.o");
+		exit(0);
+	}
+	struct timespec startTime, endTime; // <- what is this???
 	int i;
 	int numberOfCoordinates = atoi(argv[2]);
-	
-	if (readFile == NULL)
-	{
-		perror("Error while opening the file");
-	}
 	//Na doume an o xronos einai topothetimenos swsta
 	//mporei na xreiazetai na to valoume mesa sto crisimo tmima
-    #pragma omp parallel private(tid)
-    {
-	#pragma omp master
-	clock_gettime(CLOCK_MONOTONIC, &startTime);
-	
-	for (i = 0; i < numberOfCoordinates; i++){
-        	tid = omp_get_thread_num();
-        	printf("Hello world from thread %d\n", tid);
-	}
+	#pragma omp parallel private(tid)
+	{
+		/* 
+		*	//testing the threads
+		*	tid = omp_get_thread_num();
+		*	printf("Hello world from thread %d\n", tid);
+		*/
+
 		#pragma omp master
 		{
-		clock_gettime(CLOCK_MONOTONIC, &endTime);
-		printf("Tasos sad\n");
+			clock_gettime(CLOCK_MONOTONIC, &startTime);
 		}
-    }
-    return 0;
+
+		#pragma omp master
+		{
+			clock_gettime(CLOCK_MONOTONIC, &endTime);
+			printf("Tasos sad\n");
+		}
+	}
+	return 0;
 }
+
+
