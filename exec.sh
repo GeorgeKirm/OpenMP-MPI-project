fb188f843c7335a8c65b48dcbce180e09af5ce1e
vamartid <vamartid@yandex.com>
2016-04-10 13:40:22 +0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -6,5 +6,4 @@
 #PBS -l walltime=00:15:00
 export OMP_NUM_THREADS=12
 echo "working direcory is $PWD"
-cd /home/vamartid/Desktop/OpenMP-MPI-project-master
 mpirun ./examineParallel.o -1 -1 datafile -1 -1
