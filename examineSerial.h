1ca6e4a83eb5ea6f096843f91147006e39d5a3cc
alouvoul <alouvoul@users.noreply.github.com>
2016-05-15 15:39:33 +0300

    Update examineSerial.h

--- examineSerial.h
+++ examineSerial.h
@@ -5,10 +5,12 @@
 #include <string.h>
 #define DOWNLIMIT 12
 #define UPLIMIT 30
+#define LAST_RANK 6
+#define BITS_LINE 10
+#define MALLOC_SIZE 3000000 //100000 sugkrouseis
 
 void printTime(struct timespec,struct timespec);
 
 void checker(char**);
 
-int checkerSer(char**, char*, size_t);
-
+int checkerSer(char*, size_t);
